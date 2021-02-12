fdadcddb8d9e7146eaf45974f405739a67dbf3fb
Robert Rayburn <robert.rayburn@core10.io>
2021-02-11 22:52:58 -0600

    testing out basic math in r

---
+++ BasicMath.r
@@ -0,0 +1,41 @@
+1+1
+1+2+3
+7-5
+8+9-6
+
+3*4
+4*3*2
+
+4/2
+4/3
+
+# PEMDAS
+
+4*6 + 5 #29
+4*(6 + 5) #44
+
+# Preferred over =
+x <- 2
+x
+y = 5
+y
+
+z <- 3
+z
+
+# chain assignment
+a <- b <- 7
+a
+b
+
+assign("j", 4)
+j
+
+# Remove variable / De-assign
+rm(j)
+j # Gets an error
+
+theVariable <- 17
+theVariable
+THEVARIABLE # error
+
--- first.r
+++ first.r
@@ -1,1 +1,13 @@
 1+1
+2+2
+
+library(ggplot2)
+require(coefplot)
+ggplot
+
+# control + enter to install
+# install.packages("useful")
+require(useful)
+
+# unload package
+# detach("package:useful")
